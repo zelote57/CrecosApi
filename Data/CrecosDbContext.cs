43abdf2171b439ff0aeb9f0f77ae2151c67deb7a
Gustavo Lozada <zelote57@gmail.com>
2024-02-16 21:15:36 -0500

    add db context

---
+++ Data/CrecosDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CrecosApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrecosApi.Data
+{
+    public class CrecosDbContext : DbContext
+    {
+        public CrecosDbContext()
+        {            
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalle> DetallesDePedido { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
